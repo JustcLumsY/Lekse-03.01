9e529ea5c2c63ddb36ee9f4bd3bc1fa99d0f4e39
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2022-01-03 12:58:53 +0100

    Add project files.

---
+++ Lekse 03.01.2022/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lekse_03._01._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Installere visual studio(hvis dere ikke har gjort det enda)
+            //2.Se videoene konsoll-app og verdi og referansetyper og gjøre oppgavene som tilhører dem på moodle
+            //3.Løs Oppgave:
+            //Lag en konsoll - applikasjon som har to variabler av typen string.
+        string predefined = "Dette er oppgaven til første time";
+        string userInput = Console.ReadLine();  //(dette er en tom string).
+
+        Console.WriteLine(predefined, userInput);
+           
+            //Les inn bruker - input i konsollen som avgjør hva userInput skal være.
+            //Skriv deretter ut i konsollen verdiene til variablene predefined og userInput.
+        }
+
+
+    }
+}
